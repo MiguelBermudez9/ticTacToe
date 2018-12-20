9a32487ebe4b3306872e5be8286e9f64aa1eb6f7
miguel.bermudez <miguel.bermudez@MU-C1-12.mullen.pvt.k12.co.us>
2018-12-20 10:50:20 -0700

    initial commit

---
+++ Java Game/src/ticTacToe.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class ticTacToe
+	{
+
+		public static void main(String[] args)
+			{
+				display();
+			}
+		public static void display()
+		{
+			System.out.println(" |     |     |     |   ");
+			System.out.println(" |     |     |     |   ");
+			System.out.println(" |     |     |     |   ");
+			System.out.println("_|_____|_____|_____|__");
+			System.out.println(" |     |     |     |   ");
+			System.out.println(" |     |     |     |   ");
+			System.out.println(" |     |     |     |   ");
+			System.out.println("_|_____|_____|_____|__");
+			System.out.println(" |     |     |     |   ");
+			System.out.println(" |     |     |     |   ");
+			System.out.println(" |     |     |     |   ");
+			System.out.println("_|_____|_____|_____|__");
+			System.out.println(" |     |     |     |   ");
+		}
+
+	}
